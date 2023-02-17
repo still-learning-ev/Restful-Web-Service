749a2f24f54744fac5afc1371b15451cd40d920f
zeeshan <through.learning@gmail.com>
2023-02-17 15:31:42 +0530

    Post request to add user with enhanced and proper response status

--- src/main/java/com/zeeshan/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/zeeshan/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,38 +1,46 @@
 package com.zeeshan.rest.webservices.restfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
 
 	private UserDaoService service;
-	
-	
+
 	// Retrieve the details of all users
-	
+
 	public UserResource(UserDaoService service) {
 		super();
 		this.service = service;
 	}
 
-	@GetMapping(path="/users")
-	public List<User> retrieveAllUsers(){
+	@GetMapping(path = "/users")
+	public List<User> retrieveAllUsers() {
 		return service.findAll();
 	}
-	
-	@GetMapping(path="/users/{id}")
-	public User retrieveUser(@PathVariable int id){
+
+	@GetMapping(path = "/users/{id}")
+	public User retrieveUser(@PathVariable int id) {
 		return service.findOne(id);
 	}
-	
-	@PostMapping(path="/users")
-	public void createUser(@RequestBody User user) {
-		service.saveUser(user);
+
+	@PostMapping(path = "/users")
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		User savedUser = service.saveUser(user);
+		// Post request response is now 201
+		// We furthure improve this to return location or url
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId())
+				.toUri();
+		return ResponseEntity.created(location).build();
 	}
 }
